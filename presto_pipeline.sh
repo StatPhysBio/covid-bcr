e70110e7d5df2dd655b9ae96299665507006a863
Z. Montague <zacmon@mox1.hyak.local>
2020-04-04 19:55:13 -0700

    added python script to pipeline

--- presto_pipeline.sh
+++ presto_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=presto
+#SBATCH --job-name=covid_preprocess
 #SBATCH -p stf
 #SBATCH -A stf
 #SBATCH --nodes=1
@@ -7,8 +7,12 @@
 #SBATCH --ntasks-per-node=28
 #SBATCH --time=5-00:00:00
 
-#INPUTDIR=${2}
+# TODO Take in a working directory
+#      Take in a conda directory for error correction
 SCRATCHDIR=/gscratch/stf/zachmon/
+CONDADIR=${SCRATCHDIR}miniconda3/
+CONDAENVDIR=${CONDADIR}envs/covid-bcr
+RUNCONDA=${CONDADIR}etc/profile.d/conda.sh
 SOFTWAREDIR=${SCRATCHDIR}software/presto-0.5.13/bin/
 COVIDDIR=${SCRATCHDIR}covid/
 ALIGNDIR=${COVIDDIR}aligned/
@@ -16,42 +20,55 @@ FILTERDIR=${COVIDDIR}filtered/
 TRIMDIR=${COVIDDIR}masked/
 DEDUPDIR=${COVIDDIR}collapsed/
 PRIMERDIR=${COVIDDIR}data/primers/
+DEDUPONELINE=${COVIDDIR}oneline_collapsed/
+ERRORDIR=${COVIDDIR}error_corrected/
 
-usage() { echo "Usage: $0 [-s 1-18][-a] [-f] [-m] [-c]" 1>&2; exit 1; }
-while getopts ":s:afmc" arg; do
+usage() { echo "Usage: $0 [-s 1-18][-a] [-f] [-m] [-c] ][-e] [-h]" 1>&2; exit 1; }
+while getopts ":s:afmceh" arg; do
     case $arg in
         s)
             SAMPLENUM=${OPTARG}
             INPUTDIR=${COVIDDIR}data/sample_${SAMPLENUM}/
             FILENAME=S${SAMPLENUM}
             ;;
-        a)
-            ASSEMBLE=true
-            ;;
-        f)
-            FILTER=true
-            ;;
-        m)
-            MASK=true
-            ;;
-        c)
-            COLLAPSE=true
-            ;;
-        *) 
-            usage
-            ;;
+        a) ASSEMBLE= true;;
+        f) FILTER=true;;
+        m) MASK=true;;
+        c) COLLAPSE=true;;
+        e) ERRORCORRECT=true;;
+        : ) echo -e "Missing argument for -$OPTARG\n" >&2; exit 1;;
+        * ) echo -e "Unimplemented option: -$OPTARG\n" >&2; exit 1;;
+        h)  usage && exit 0;;
     esac
 done
 shift $((OPTIND-1))
+
+#  Ensure sample num is an integer.
+if ! [[ "$SAMPLENUM" =~ ^[0-9]+$ ]]; then
+    echo "Sample num must be an interger."
+    usage
+    exit 1
+fi
+
+#  Ensure sample num is between 1 and 18.
+#  TODO Update when more samples become available.
+if [[ "$SAMPLENUM" -gt 18 ]]; then
+    echo "Sample num is between 1 and 18." >&2
+    usage
+    exit 1
+fi
 
 echo "SAMPLE NUM $SAMPLENUM"
 echo "ASSEMBLE $ASSEMBLE"
 echo "FILTER $FILTER"
 echo "MASK $MASK"
 echo "COLLAPSE $COLLAPSE"
-#if [ -z "${s}" ] || [ -z "${a}" ] || [ -z "${f}" ] || [ -z "${m}" ] || [ -z "${c}" ]; then
+echo "ERROR CORRECT $ERRORCORRECT"
+
+#if [ -z "${a}" ] && [ -z "${f}" ] && [ -z "${m}" ] && [ -z "${c}" ]; then
 #    usage
 #fi
+
 #========== Assembling Pairs ==========
 if [ "$ASSEMBLE" = true ]; then
     if [ ! -d "$ALIGNDIR" ]; then
@@ -74,6 +91,7 @@ if [ "$FILTER" = true ]; then
 
     #  Filter sequences using a QScore of 30.
     ${SOFTWAREDIR}FilterSeq.py quality -s ${ALIGNDIR}${FILENAME}_assemble-pass.fastq \
+                                       --fasta \
                                        -q 30 \
                                        --outname ${FILTERDIR}30_${FILENAME} \
                                        --log ${FILTERDIR}filter_30_S${SAMPLENUM}.log
@@ -85,14 +103,14 @@ if [ "$MASK" = true ]; then
     fi
 
     #  Mask V primers.
-    ${SOFTWAREDIR}MaskPrimers.py score -s ${FILTERDIR}30_${FILENAME}_quality-pass.fastq \
+    ${SOFTWAREDIR}MaskPrimers.py score -s ${FILTERDIR}30_${FILENAME}_quality-pass.fasta \
                                        -p ${PRIMERDIR}vprimers.fa \
                                        --mode mask --pf VPRIMER \
                                        --outname ${TRIMDIR}${FILENAME}-FWD \
                                        --log ${TRIMDIR}${SAMPLENUM}PV.log
 
     #  Cut C primer.
-    ${SOFTWAREDIR}MaskPrimers.py score -s ${TRIMDIR}${FILENAME}-FWD_primers-pass.fastq \
+    ${SOFTWAREDIR}MaskPrimers.py score -s ${TRIMDIR}${FILENAME}-FWD_primers-pass.fasta \
                                        -p ${PRIMERDIR}cprimers.fa \
                                        --mode cut --revpr --pf CPRIMER \
                                        --outname ${FILENAME}-REV \
@@ -103,10 +121,41 @@ if [ "$COLLAPSE" = true ]; then
     if [ ! -d "$DEDUPDIR" ]; then
       mkdir $DEDUPDIR
     fi
+    if [ ! -d "$DEDUPONELINE" ]; then
+      mkdir $DEDUPONELINE
+    fi
 
     #  Deduplicate data.
-    ${SOFTWAREDIR}CollapseSeq.py -s ${TRIMDIR}${FILENAME}-REV_primers-pass.fastq \
+    ${SOFTWAREDIR}CollapseSeq.py -s ${TRIMDIR}${FILENAME}-REV_primers-pass.fasta \
                                  -n 0 \
                                  --uf CPRIMER --cf VPRIMER --act set --inner \
                                  --outname ${DEDUPDIR}unique_${FILENAME}
+   
+    #  Convert sequence in fasta file to one line.
+    fasta_formatter -i ${DEDUPDIR}unique_${FILENAME}_collapse-unique.fasta -o ${DEDUPONELINE}${FILENAME}_collapse-unique.fasta
+fi
+
+#========== Error Correct Singleton Sequences ==========
+if [ "$ERRORCORRECT" = true ]; then
+    if [ ! -d "$ERRORDIR" ]; then
+      mkdir $ERRORDIR
+    fi
+
+    #  Export conda functions to be available to subshell.
+    source ${RUNCONDA} 
+    
+    #  Create conda environment if necessary.
+    if [ ! -d "${CONDAENVDIR}" ]; then
+      echo "Conda environment covid-bcr does not exist. Creating now."
+      conda env create -f env.yml
+    fi
+    
+    #  Change to conda environment.
+    conda activate covid-bcr
+    
+    #  Error correct data.
+    python error_correct.py ${DEDUPONELINE}${FILENAME}_collapse-unique.fasta > ${ERRORDIR}${FILENAME}_corrected.fasta
+    
+    #  Deactivate conda environment.
+    conda deactivate
 fi
