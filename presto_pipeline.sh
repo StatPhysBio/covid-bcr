7075394897425217af857db58c29800f4543e651
Z. Montague <zacmon@mox1.hyak.local>
2020-05-13 13:33:05 -0700

    fixed script file

--- presto_pipeline.sh
+++ presto_pipeline.sh
@@ -14,52 +14,51 @@ CONDADIR=${SCRATCHDIR}miniconda3/
 CONDAENVDIR=${CONDADIR}envs/covid-bcr
 RUNCONDA=${CONDADIR}etc/profile.d/conda.sh
 SOFTWAREDIR=${SCRATCHDIR}software/presto-0.5.13/bin/
-COVIDDIR=${SCRATCHDIR}covid/
+COVIDDIR=${SCRATCHDIR}covid/round2data/
 ALIGNDIR=${COVIDDIR}aligned/
 FILTERDIR=${COVIDDIR}filtered/
 TRIMDIR=${COVIDDIR}masked/
 DEDUPDIR=${COVIDDIR}collapsed/
-PRIMERDIR=${COVIDDIR}data/primers/
+PRIMERDIR=${COVIDDIR}raw_data/primers/
 DEDUPONELINE=${COVIDDIR}oneline_collapsed/
 ERRORDIR=${COVIDDIR}error_corrected/
 
-usage() { echo "Usage: $0 [-s 1-18][-a] [-f] [-m] [-c] ][-e] [-h]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-s <string>] [-a] [-f] [-m] [-c] ][-e] [-h]" 1>&2; exit 1; }
 while getopts ":s:afmceh" arg; do
     case $arg in
-        s)
-            SAMPLENUM=${OPTARG}
-            INPUTDIR=${COVIDDIR}data/sample_${SAMPLENUM}/
-            FILENAME=S${SAMPLENUM}
-            ;;
-        a) ASSEMBLE= true;;
+        s) SAMPLENUM=${OPTARG}
+           INPUTDIR=${COVIDDIR}raw_data/sample_${SAMPLENUM}/
+           FILENAME=S${SAMPLENUM}
+           echo "Input dir is ${INPUTDIR}";;
+        a) ASSEMBLE=true;;
         f) FILTER=true;;
         m) MASK=true;;
         c) COLLAPSE=true;;
         e) ERRORCORRECT=true;;
+        h)  usage;  exit 0;;
+        \? ) echo "Unknown option: -$OPTARG\n" >&2; exit 1;;
         : ) echo -e "Missing argument for -$OPTARG\n" >&2; exit 1;;
         * ) echo -e "Unimplemented option: -$OPTARG\n" >&2; exit 1;;
-        h)  usage && exit 0;;
     esac
 done
 shift $((OPTIND-1))
 
 #  Ensure sample num is an integer.
-if ! [[ "$SAMPLENUM" =~ ^[0-9]+$ ]]; then
-    echo "Sample num must be an interger."
-    usage
-    exit 1
-fi
+#if ! [[ "$SAMPLENUM" =~ ^[0-9]+$ ]]; then
+#    echo "Sample num must be an interger."
+#    usage
+#    exit 1
+#fi
 
 #  Ensure sample num is between 1 and 18.
 #  TODO Update when more samples become available.
-if [[ "$SAMPLENUM" -gt 18 ]]; then
-    echo "Sample num is between 1 and 18." >&2
-    usage
-    exit 1
-fi
-
+#if [[ "$SAMPLENUM" -gt 18 ]]; then
+#    echo "Sample num is between 1 and 18." >&2
+#    usage
+#    exit 1
+#fi
 echo "SAMPLE NUM $SAMPLENUM"
-echo "ASSEMBLE $ASSEMBLE"
+echo "ASSEMBLE ${ASSEMBLE}"
 echo "FILTER $FILTER"
 echo "MASK $MASK"
 echo "COLLAPSE $COLLAPSE"
@@ -70,14 +69,14 @@ echo "ERROR CORRECT $ERRORCORRECT"
 #fi
 
 #========== Assembling Pairs ==========
-if [ "$ASSEMBLE" = true ]; then
+if [ "${ASSEMBLE}" = true ]; then
     if [ ! -d "$ALIGNDIR" ]; then
       mkdir $ALIGNDIR
     fi
-
+    echo "ASSEMBLING!"
     #  Assemble pairs.
-    ${SOFTWAREDIR}AssemblePairs.py align -1 ${INPUTDIR}${SAMPLENUM}-IGG_1.fastq \
-                                         -2 ${INPUTDIR}${SAMPLENUM}-IGG_2.fastq \
+    ${SOFTWAREDIR}AssemblePairs.py align -1 ${INPUTDIR}IgG${SAMPLENUM}_1.fastq \
+                                         -2 ${INPUTDIR}IgG${SAMPLENUM}_2.fastq \
                                          --coord illumina \
                                          --rc tail \
                                          --outname ${ALIGNDIR}${FILENAME} \
@@ -88,7 +87,7 @@ if [ "$FILTER" = true ]; then
     if [ ! -d "$FILTERDIR" ]; then
       mkdir $FILTERDIR
     fi
-
+    echo "FILTERING!"
     #  Filter sequences using a QScore of 30.
     ${SOFTWAREDIR}FilterSeq.py quality -s ${ALIGNDIR}${FILENAME}_assemble-pass.fastq \
                                        --fasta \
@@ -101,7 +100,7 @@ if [ "$MASK" = true ]; then
     if [ ! -d "$TRIMDIR" ]; then
       mkdir $TRIMDIR
     fi
-
+    echo "MASKING!"
     #  Mask V primers.
     ${SOFTWAREDIR}MaskPrimers.py score -s ${FILTERDIR}30_${FILENAME}_quality-pass.fasta \
                                        -p ${PRIMERDIR}vprimers.fa \
@@ -124,10 +123,10 @@ if [ "$COLLAPSE" = true ]; then
     if [ ! -d "$DEDUPONELINE" ]; then
       mkdir $DEDUPONELINE
     fi
-
+    echo "DEDUPLICATING!"
     #  Deduplicate data.
     ${SOFTWAREDIR}CollapseSeq.py -s ${TRIMDIR}${FILENAME}-REV_primers-pass.fasta \
-                                 -n 0 \
+                                 -n 20 \
                                  --uf CPRIMER --cf VPRIMER --act set --inner \
                                  --outname ${DEDUPDIR}unique_${FILENAME}
    
